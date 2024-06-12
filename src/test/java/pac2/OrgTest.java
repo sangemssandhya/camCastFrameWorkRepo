bca90c73fc101377e995fcc21aa4cbd07123cc6f
sangemssandhya <152694626+sangemssandhya@users.noreply.github.com>
2024-06-12 15:57:14 +0530

    ContactORg OrgTEST added

---
+++ src/test/java/pac2/OrgTest.java
@@ -0,0 +1,17 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void createOrgTest()
+	{
+		System.out.println("execute org test");
+	}
+	@Test
+	public void modifyorgTest()
+	{
+		System.out.println("execute modifyorgtest");
+	}
+
+}
